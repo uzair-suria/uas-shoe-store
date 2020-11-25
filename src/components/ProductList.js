aaa27653be31f9a674af04fabe893295d6ab3e37
Uzair Suria <uzair.a.suria@outlook.com>
2020-11-26 00:42:57 +0500

    Create ProductList.js

---
+++ src/components/ProductList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ProductList = () => {
+	return (
+		<div>
+			<>
+				ProductList Component: To be used to display filtered/unfiltered
+				products
+			</>
+		</div>
+	);
+};
+
+export default ProductList;

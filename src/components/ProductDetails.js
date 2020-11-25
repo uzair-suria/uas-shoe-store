aa8eb95d515f02519a9ada6c215cedc15551808a
Uzair Suria <uzair.a.suria@outlook.com>
2020-11-26 00:42:53 +0500

    Create ProductDetails.js

---
+++ src/components/ProductDetails.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ProductDetails = () => {
+	return (
+		<div>
+			<>
+				ProductDetails component to show complete product details of a single
+				product
+			</>
+		</div>
+	);
+};
+
+export default ProductDetails;

c84d991aee3ab88e6b4c7cfd03023edde52dc3cf
Uzair Suria <uzair.a.suria@outlook.com>
2020-11-26 20:50:23 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Layout from './components/Layout';
 
 function App() {
 	return (
 		<div className="App">
-			<h1 style={{ textAlign: 'center' }}>Website Under Development</h1>
+			<Layout />
 		</div>
 	);
 }

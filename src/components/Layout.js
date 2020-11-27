827a6235f494f572d23d9d48612128e62663aab2
Uzair Suria <uzair.a.suria@outlook.com>
2020-11-27 22:01:15 +0500

    Update Layout.js
    Added side drawer, appBar, and theme toggler. Dummy text to fill the space needs to be updated to actual text

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,6 @@
 import {
 	AppBar,
+	Button,
 	CssBaseline,
 	Divider,
 	Drawer,
@@ -11,53 +12,39 @@ import {
 	ListItemText,
 	makeStyles,
 	Toolbar,
+	Tooltip,
 	Typography,
 	useTheme,
 } from '@material-ui/core';
-import { grey } from '@material-ui/core/colors';
+import {} from '@material-ui/core/colors';
 import InboxIcon from '@material-ui/icons/Inbox';
 import MailIcon from '@material-ui/icons/Mail';
 import MenuIcon from '@material-ui/icons/Menu';
+import BrightnessHighIcon from '@material-ui/icons/BrightnessHigh';
+import Brightness4Icon from '@material-ui/icons/Brightness4';
 import React, { useState } from 'react';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 
 const drawerWidth = 240;
 
-// const useStyles = makeStyles((theme) => ({
-// 	root: {
-// 		display: 'flex',
-// 	},
-// 	drawer: {
-// 		[theme.breakpoints.up('sm')]: {
-// 			width: drawerWidth,
-// 			flexShrink: 0,
-// 		},
-// 	},
-// 	appBar: {
-// 		[theme.breakpoints.up('sm')]: {
-// 			width: `calc(100% - ${drawerWidth}px)`,
-// 			marginLeft: drawerWidth,
-// 		},
-// 	},
-// 	menuButton: {
-// 		marginRight: theme.spacing(2),
-// 		[theme.breakpoints.up('sm')]: {
-// 			display: 'none',
-// 		},
-// 	},
-// 	// necessary for content to be below app bar
-// 	toolbar: theme.mixins.toolbar,
-// 	drawerPaper: {
-// 		width: drawerWidth,
-// 	},
-// 	content: {
-// 		flexGrow: 1,
-// 		padding: theme.spacing(3),
-// 	},
-// }));
+const darkTheme = createMuiTheme({
+	palette: {
+		type: 'dark',
+	},
+});
+
+const lightTheme = createMuiTheme({
+	palette: {
+		type: 'light',
+	},
+});
 
 const useStyles = makeStyles((theme) => ({
 	root: {
 		display: 'flex',
+	},
+	appBarNav: {
+		marginLeft: 'auto',
 	},
 	drawer: {
 		[theme.breakpoints.up('sm')]: {
@@ -65,15 +52,8 @@ const useStyles = makeStyles((theme) => ({
 			flexShrink: 0,
 		},
 	},
-	drawerTitle: {
+	toolbarTitle: {
 		padding: 5,
-		textAlign: 'center',
-		color: grey[700],
-	},
-	drawerSubtitle: {
-		textAlign: 'right',
-		color: grey[400],
-		paddingRight: 5,
 	},
 	appBar: {
 		[theme.breakpoints.up('sm')]: {
@@ -103,6 +83,7 @@ const Layout = (props) => {
 	const classes = useStyles();
 	const theme = useTheme();
 	const [mobileOpen, setMobileOpen] = useState(false);
+	const [darkMode, setDarkMode] = useState(false);
 
 	const handleDrawerToggle = () => {
 		setMobileOpen(!mobileOpen);
@@ -111,16 +92,19 @@ const Layout = (props) => {
 	const drawer = (
 		<div>
 			<div className={classes.toolbar}>
-				<Typography variant="h5" className={classes.drawerTitle}>
-					Shoe Store
-				</Typography>
-				<Typography
-					variant="caption"
-					component="h6"
-					className={classes.drawerSubtitle}
-				>
-					Built by Uzair Suria
-				</Typography>
+				<div className={classes.toolbarTitle}>
+					<Typography variant="h5" color="textSecondary" align="center">
+						Shoe Store
+					</Typography>
+					<Typography
+						variant="caption"
+						component="h6"
+						color="textSecondary"
+						align="right"
+					>
+						Built by Uzair Suria
+					</Typography>
+				</div>
 			</div>
 			<Divider />
 			<List>
@@ -151,88 +135,115 @@ const Layout = (props) => {
 		window !== undefined ? () => window().document.body : undefined;
 
 	return (
-		<div className={classes.root}>
-			<CssBaseline />
-			<AppBar position="fixed" className={classes.appBar}>
-				<Toolbar>
-					<IconButton
-						color="inherit"
-						aria-label="open drawer"
-						edge="start"
-						onClick={handleDrawerToggle}
-						className={classes.menuButton}
-					>
-						<MenuIcon />
-					</IconButton>
-					<Hidden smUp>
-						<Typography variant="h6" noWrap>
-							Shoe Store
-						</Typography>
+		<ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+			<div className={classes.root}>
+				<CssBaseline />
+				<AppBar position="fixed" className={classes.appBar}>
+					<Toolbar>
+						<IconButton
+							color="inherit"
+							aria-label="open drawer"
+							edge="start"
+							onClick={handleDrawerToggle}
+							className={classes.menuButton}
+						>
+							<MenuIcon />
+						</IconButton>
+						<Hidden smUp>
+							<Typography variant="h6" noWrap>
+								Shoe Store
+							</Typography>
+						</Hidden>
+						<div className={classes.appBarNav}>
+							<Tooltip title="About us">
+								<Button color="inherit" href="/About">
+									About
+								</Button>
+							</Tooltip>
+							<Tooltip title="Contact Info">
+								<Button color="inherit" href="/Contact">
+									Contact
+								</Button>
+							</Tooltip>
+							<Tooltip title="Toggle light/dark theme">
+								<IconButton
+									color="inherit"
+									aria-label="theme toggle"
+									component="span"
+									onClick={() => {
+										setDarkMode(!darkMode);
+									}}
+								>
+									{darkMode ? <BrightnessHighIcon /> : <Brightness4Icon />}
+								</IconButton>
+							</Tooltip>
+						</div>
+					</Toolbar>
+				</AppBar>
+				<nav className={classes.drawer} aria-label="mailbox folders">
+					{/* Temporary drawer for small screens (smaller than 'xs') */}
+					<Hidden smUp implementation="css">
+						<Drawer
+							container={container}
+							variant="temporary"
+							anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+							open={mobileOpen}
+							onClose={handleDrawerToggle}
+							classes={{ paper: classes.drawerPaper }}
+							ModalProps={{ keepMounted: true }}
+						>
+							{drawer}
+						</Drawer>
 					</Hidden>
-				</Toolbar>
-			</AppBar>
-			<nav className={classes.drawer} aria-label="mailbox folders">
-				{/* Temporary drawer for small screens (smaller than 'xs') */}
-				<Hidden smUp implementation="css">
-					<Drawer
-						container={container}
-						variant="temporary"
-						anchor={theme.direction === 'rtl' ? 'right' : 'left'}
-						open={mobileOpen}
-						onClose={handleDrawerToggle}
-						classes={{ paper: classes.drawerPaper }}
-						ModalProps={{ keepMounted: true }}
-					>
-						{drawer}
-					</Drawer>
-				</Hidden>
 
-				{/* Permanent drawer for Large screens (greater than 'xs') */}
-				<Hidden xsDown implementation="css">
-					<Drawer
-						classes={{
-							paper: classes.drawerPaper,
-						}}
-						variant="permanent"
-						open
-					>
-						{drawer}
-					</Drawer>
-				</Hidden>
-			</nav>
-			<main className={classes.content}>
-				<div className={classes.toolbar} />
-				<Typography paragraph>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-					eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
-					dolor purus non enim praesent elementum facilisis leo vel. Risus at
-					ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum
-					quisque non tellus. Convallis convallis tellus id interdum velit
-					laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed
-					adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
-					integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
-					eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
-					quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
-					vivamus at augue. At augue eget arcu dictum varius duis at consectetur
-					lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa sapien
-					faucibus et molestie ac.
-				</Typography>
-				<Typography paragraph>
-					Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
-					ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
-					elementum integer enim neque volutpat ac tincidunt. Ornare suspendisse
-					sed nisi lacus sed viverra tellus. Purus sit amet volutpat consequat
-					mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis
-					risus sed vulputate odio. Morbi tincidunt ornare massa eget egestas
-					purus viverra accumsan in. In hendrerit gravida rutrum quisque non
-					tellus orci ac. Pellentesque nec nam aliquam sem et tortor. Habitant
-					morbi tristique senectus et. Adipiscing elit duis tristique
-					sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-					eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-					posuere sollicitudin aliquam ultrices sagittis orci a.
-				</Typography>
-			</main>
-		</div>
+					{/* Permanent drawer for Large screens (greater than 'xs') */}
+					<Hidden xsDown implementation="css">
+						<Drawer
+							classes={{
+								paper: classes.drawerPaper,
+							}}
+							variant="permanent"
+							open
+						>
+							{drawer}
+						</Drawer>
+					</Hidden>
+				</nav>
+				<main className={classes.content}>
+					<div className={classes.toolbar} />
+					<Typography paragraph>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+						eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
+						dolor purus non enim praesent elementum facilisis leo vel. Risus at
+						ultrices mi tempus imperdiet. Semper risus in hendrerit gravida
+						rutrum quisque non tellus. Convallis convallis tellus id interdum
+						velit laoreet id donec ultrices. Odio morbi quis commodo odio aenean
+						sed adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
+						integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
+						eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
+						quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
+						vivamus at augue. At augue eget arcu dictum varius duis at
+						consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
+						donec massa sapien faucibus et molestie ac.
+					</Typography>
+					<Typography paragraph>
+						Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
+						ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
+						elementum integer enim neque volutpat ac tincidunt. Ornare
+						suspendisse sed nisi lacus sed viverra tellus. Purus sit amet
+						volutpat consequat mauris. Elementum eu facilisis sed odio morbi.
+						Euismod lacinia at quis risus sed vulputate odio. Morbi tincidunt
+						ornare massa eget egestas purus viverra accumsan in. In hendrerit
+						gravida rutrum quisque non tellus orci ac. Pellentesque nec nam
+						aliquam sem et tortor. Habitant morbi tristique senectus et.
+						Adipiscing elit duis tristique sollicitudin nibh sit. Ornare aenean
+						euismod elementum nisi quis eleifend. Commodo viverra maecenas
+						accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam
+						ultrices sagittis orci a.
+					</Typography>
+				</main>
+			</div>
+		</ThemeProvider>
 	);
 };
 

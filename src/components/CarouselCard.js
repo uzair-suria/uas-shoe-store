99c22cfa7a762a2103d2b451fd7d4541a6171743
Uzair Suria <uzair.a.suria@outlook.com>
2020-11-30 16:25:40 +0500

    Update CarouselCard.js
    Added link to product details

--- src/components/CarouselCard.js
+++ src/components/CarouselCard.js
@@ -8,6 +8,7 @@ import {
 	Typography,
 } from '@material-ui/core';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { shoeData } from '../data/shoeData';
 import useWindowSize from '../hooks/useWindowSize';
 
@@ -28,45 +29,47 @@ const CarouselCard = ({ productId }) => {
 
 	const classes = useStyles();
 	return (
-		<Card className={classes.root}>
-			<CardActionArea>
-				<CardMedia
-					component="img"
-					alt={productId}
-					height={windowSize.width ? windowSize.width * 0.45 : ''}
-					src={
-						require(`../images/${shoeData[productId].gender}/${productId}/01.jpg`)
-							.default
-					}
-					title={shoe.tagline}
-					style={{
-						overflow: 'hidden',
-						margin: `-${100 / windowSize.width}px 0px 0px 0px`,
-						borderBottom: '0px solid gray',
-					}}
-				/>
-				<Divider />
-				<CardContent>
-					<Typography
-						gutterBottom
-						variant="h6"
-						component="h2"
-						color="textPrimary"
-					>
-						{shoe.name}
-					</Typography>
-					<Typography variant="body1" color="textSecondary" component="p">
-						{shoe.gender === 'male' ? `Men's` : `Women's`} Shoe
-					</Typography>
-					<Typography variant="body2" color="textSecondary" component="p">
-						{shoe.tagline}
-					</Typography>
-					<Typography align="right" className={classes.price}>
-						${shoe.price}
-					</Typography>
-				</CardContent>
-			</CardActionArea>
-		</Card>
+		<Link to={`/productList/${productId}`} style={{ textDecoration: 'none' }}>
+			<Card className={classes.root}>
+				<CardActionArea>
+					<CardMedia
+						component="img"
+						alt={productId}
+						height={windowSize.width ? windowSize.width * 0.45 : ''}
+						src={
+							require(`../images/${shoeData[productId].gender}/${productId}/01.jpg`)
+								.default
+						}
+						title={shoe.tagline}
+						style={{
+							overflow: 'hidden',
+							margin: `-${100 / windowSize.width}px 0px 0px 0px`,
+							borderBottom: '0px solid gray',
+						}}
+					/>
+					<Divider />
+					<CardContent>
+						<Typography
+							gutterBottom
+							variant="h6"
+							component="h2"
+							color="textPrimary"
+						>
+							{shoe.name}
+						</Typography>
+						<Typography variant="body1" color="textSecondary" component="p">
+							{shoe.gender === 'male' ? `Men's` : `Women's`} Shoe
+						</Typography>
+						<Typography variant="body2" color="textSecondary" component="p">
+							{shoe.tagline}
+						</Typography>
+						<Typography align="right" className={classes.price}>
+							${shoe.price}
+						</Typography>
+					</CardContent>
+				</CardActionArea>
+			</Card>
+		</Link>
 	);
 };
 
